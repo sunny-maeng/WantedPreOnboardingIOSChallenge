5c18ce5de5d4c4ade0f118ad35b0de1933b1edb4
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-03-01 17:23:54 +0900

    Initial Commit

---
+++ WantedPreOnboardingIOSChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WantedPreOnboardingIOSChallenge
+//
+//  Created by 맹선아 on 2023/03/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
